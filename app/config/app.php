3fc0f38477fd41f1cd2a053b271c94e40b444ad8
alexgzhou <alexgzhou@163.com>
2014-11-04 02:49:16 +0800

    2. laravel-4-migrations
    #1#
    app\config\database.php
    #2#
    composer.json:
    $ composer update --dev
    #3#
    app\config\app.php
    #4#
    $ php artisan migrate:install
    #5#
    $ php artisan generate:migration create_users_table
    --fields="email:string, username:string, password:string"
    #6#
    $ php artisan migrate

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,8 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
+		'Way\Generators\GeneratorsServiceProvider',	// 2. laravel-4-migrations
+
 	),
 
 	/*
