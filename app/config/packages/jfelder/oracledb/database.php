abd5da6092ea9c4215c0d164033afa6e4d1a6487
alexgzhou <alexgzhou@163.com>
2014-11-09 23:55:13 +0800

    jfelder/oracledb

---
+++ app/config/packages/jfelder/oracledb/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+	'connections' => array(
+
+		'oracle' => array(
+			'driver'   => 'pdo',
+			'tns'      => '',
+			'host'     => '',
+			'port'     => '1521',
+			'database' => 'fyzxqy',
+			'username' => 'fyzxqy',
+			'password' => 'fyzxqy',
+			'charset'  => 'WE8ISO8859P1',
+			'prefix'   => '',
+			'quoting'  => false,
+		),
+
+	),
+);
